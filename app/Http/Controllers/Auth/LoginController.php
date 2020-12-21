61c51e5f2e5615f2987e9a0d9f3dcc0ad23d6686
Refinaldy Madras <refimadras.code@gmail.com>
2020-12-21 22:01:54 +0700

    Add login with email/username

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Symfony\Component\Console\Input\Input;
 
 class LoginController extends Controller
 {
@@ -27,6 +28,7 @@ class LoginController extends Controller
      * @var string
      */
     protected $redirectTo = RouteServiceProvider::HOME;
+    protected $username;
 
     /**
      * Create a new controller instance.
@@ -36,11 +38,20 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+        $this->username = $this->findUsername();
+    }
+
+    public function findUsername() 
+    {
+        $login = request()->input('credential');
+        $fieldType = filter_var($login, FILTER_VALIDATE_EMAIL) ? 'email' : 'username';
+        request()->merge([$fieldType => $login]);
+        return $fieldType;
     }
     
     public function username()
     {
-        return 'username';
+        return $this->username;
     }
 
 }
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,12 +12,13 @@
                         @csrf
 
                         <div class="form-group row">
-                            <label for="username" class="col-md-4 col-form-label text-md-right">Username</label>
+                            <label for="credential" class="col-md-4 col-form-label text-md-right">Email/Username</label>
 
                             <div class="col-md-6">
-                                <input id="username" type="username" class="form-control @error('username') is-invalid @enderror" name="username" value="{{ old('username') }}" required autocomplete="username" autofocus>
+                                <input id="credential" type="text" class="form-control @error('credential') is-invalid @enderror" 
+                                    name="credential" value="{{ old('credential') }}" required autocomplete="credential" autofocus>
 
-                                @error('username')
+                                @error('credential')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
